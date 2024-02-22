5098c0f7a9d78ea6ce024f7dcbd4ec2ae1638383
fathualr <fathualrazi@gmail.com>
2024-02-22 11:02:15 +0700

    p-3

---
+++ resources/views/list_item.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <div>
+        <h1>List Item</h1>
+        <p>Kode Item: {{$id}}</p>
+        <p>Nama Item: {{$nama}}</p>
+        <p>stok Item: {{$stok}}</p>
+        <p>harga Item: {{$harga}}</p>
+    </div>
+</html>
